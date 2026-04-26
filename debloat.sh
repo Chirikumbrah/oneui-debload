ed2cff0e6b8038047552209b44909a8b20663cc8
Chirikumbrah <dvps4k@gmail.com>
2026-04-26 19:15:35 +0300

    actualized packages list; rewrote script

--- debloat.sh
+++ debloat.sh
@@ -1,93 +1,78 @@
-#!/bin/sh
+#!/bin/bash
 
-adb shell pm disable-user --user 0 com.android.apps.tag
-adb shell pm disable-user --user 0 com.android.bookmarkprovider
-adb shell pm disable-user --user 0 com.android.chrome
-adb shell pm disable-user --user 0 com.android.dreams.basic
-adb shell pm disable-user --user 0 com.android.dreams.phototable
-adb shell pm disable-user --user 0 com.android.hotwordenrollment.okgoogle
-adb shell pm disable-user --user 0 com.android.hotwordenrollment.xgoogle
-adb shell pm disable-user --user 0 com.android.managedprovisioning
-adb shell pm disable-user --user 0 com.android.printspooler
-adb shell pm disable-user --user 0 com.android.providers.partnerbookmarks
-adb shell pm disable-user --user 0 com.android.providers.userdictionary
+PACKAGES_TO_DISABLE=(
+    android.autoinstalls.config.samsung
+    com.android.chrome
+    com.android.dreams.basic
+    com.android.dreams.phototable
+    com.android.hotwordenrollment.xgoogle
+    com.diotek.sec.lookup.dictionary
+    com.facebook.appmanager
+    com.facebook.services
+    com.facebook.system
+    com.google.android.adservices.api
+    com.google.android.apps.accessibility.voiceaccess
+    com.google.android.apps.aiwallpapers
+    com.google.android.apps.googleassistant
+    com.google.android.apps.messaging
+    com.google.android.apps.restore
+    com.google.android.apps.tachyon
+    com.google.android.as
+    com.google.android.feedback
+    com.google.android.gm
+    com.google.android.gms.supervision
+    com.google.android.googlequicksearchbox
+    com.google.android.printservice.recommendation
+    com.google.android.setupwizard
+    com.google.android.youtube
+    com.google.audio.hearing.visualization.accessibility.scribe
+    com.knox.vpn.proxyhandler
+    com.microsoft.skydrive
+    com.samsung.SMT.lang_en_us_l03
+    com.samsung.SMT.lang_ru_ru_f00
+    com.samsung.android.app.omcagent
+    com.samsung.android.app.updatecenter
+    com.samsung.android.appseparation
+    com.samsung.android.ardrawing
+    com.samsung.android.aremoji
+    com.samsung.android.aremojieditor
+    com.samsung.android.authfw
+    com.samsung.android.bbc.bbcagent
+    com.samsung.android.carkey
+    com.samsung.android.dkey
+    com.samsung.android.dqagent
+    com.samsung.android.forest
+    com.samsung.android.game.gamehome
+    com.samsung.android.game.gametools
+    com.samsung.android.game.gos
+    com.samsung.android.ipsgeofence
+    com.samsung.android.knox.analytics.uploader
+    com.samsung.android.knox.attestation
+    com.samsung.android.knox.containercore
+    com.samsung.android.knox.kpecore
+    com.samsung.android.knox.pushmanager
+    com.samsung.android.knox.zt.framework
+    com.samsung.android.mapsagent
+    com.samsung.android.mcfds
+    com.samsung.android.peripheral.framework
+    com.samsung.android.rampart
+    com.samsung.android.samsungpass
+    com.samsung.android.samsungpassautofill
+    com.samsung.android.themestore
+    com.samsung.android.visionintelligence
+    com.samsung.cmfa.AuthTouch
+    com.samsung.knox.securefolder
+    com.sec.android.app.billing
+    com.sec.android.app.vepreload
+    com.sec.android.easyMover
+    com.sec.android.mimage.avatarstickers
+    com.sec.android.soagent
+    com.sec.android.widgetapp.webmanual
+    com.swiftkey.swiftkeyconfigurator
+    com.touchtype.swiftkey
+    com.wssyncmldm
+)
 
-adb shell pm disable-user --user 0 com.diotek.sec.lookup.dictionary
-
-adb shell pm disable-user --user 0 com.facebook.appmanager
-adb shell pm disable-user --user 0 com.facebook.services
-adb shell pm disable-user --user 0 com.facebook.system
-
-adb shell pm disable-user --user 0 com.google.android.apps.tachyon
-adb shell pm disable-user --user 0 com.google.android.printservice.recommendation
-adb shell pm disable-user --user 0 com.google.android.projection.gearhead
-adb shell pm disable-user --user 0 com.google.android.tts
-adb shell pm disable-user --user 0 com.google.ar.core
-adb shell pm disable-user --user 0 com.google.audio.hearing.visualization.accessibility.scribe
-
-adb shell pm disable-user --user 0 com.osp.app.signin
-
-adb shell pm disable-user --user 0 com.samsung.SMT
-adb shell pm disable-user --user 0 com.samsung.android.accessibility.talkback
-
-adb shell pm disable-user --user 0 com.samsung.android.app.ledbackcover
-adb shell pm disable-user --user 0 com.samsung.android.mapsagent
-adb shell pm disable-user --user 0 com.samsung.android.app.routines
-adb shell pm disable-user --user 0 com.samsung.android.app.settings.bixby
-adb shell pm disable-user --user 0 com.samsung.android.app.spage
-adb shell pm disable-user --user 0 com.samsung.android.app.updatecenter
-
-adb shell pm disable-user --user 0 com.samsung.android.ardrawing
-adb shell pm disable-user --user 0 com.samsung.android.aremoji
-adb shell pm disable-user --user 0 com.samsung.android.aremojieditor
-adb shell pm disable-user --user 0 com.samsung.android.arzone
-adb shell pm disable-user --user 0 com.samsung.android.authfw
-adb shell pm disable-user --user 0 com.samsung.android.bixby.agent
-adb shell pm disable-user --user 0 com.samsung.android.bixby.wakeup
-adb shell pm disable-user --user 0 com.samsung.android.bixbyvision.framework
-
-adb shell pm disable-user --user 0 com.samsung.android.dkey
-
-adb shell pm disable-user --user 0 com.samsung.android.game.gamehome
-adb shell pm disable-user --user 0 com.samsung.android.game.gametools
-adb shell pm disable-user --user 0 com.samsung.android.game.gos
-
-adb shell pm disable-user --user 0 com.samsung.android.kidsinstaller
-adb shell pm disable-user --user 0 com.samsung.android.ipsgeofence
-
-adb shell pm disable-user --user 0 com.samsung.android.knox.containercore
-adb shell pm disable-user --user 0 com.samsung.android.knox.pushmanager
-
-adb shell pm disable-user --user 0 com.samsung.android.mateagent
-adb shell pm disable-user --user 0 com.samsung.android.mdx
-adb shell pm disable-user --user 0 com.samsung.android.mobileservice
-adb shell pm disable-user --user 0 com.samsung.android.scloud
-adb shell pm disable-user --user 0 com.samsung.android.scpm
-adb shell pm disable-user --user 0 com.samsung.android.scs
-adb shell pm disable-user --user 0 com.samsung.android.samsungpassautofill
-adb shell pm disable-user --user 0 com.samsung.android.service.tagservice
-adb shell pm disable-user --user 0 com.samsung.android.service.livedrawing
-adb shell pm disable-user --user 0 com.samsung.android.spayfw
-adb shell pm disable-user --user 0 com.samsung.android.svoiceime
-adb shell pm disable-user --user 0 com.samsung.android.visionintelligence
-adb shell pm disable-user --user 0 com.samsung.android.vtcamerasettings
-adb shell pm disable-user --user 0 com.samsung.gpuwatchapp
-adb shell pm disable-user --user 0 com.samsung.knox.securefolder
-
-adb shell pm disable-user --user 0 com.sec.android.app.billing
-adb shell pm disable-user --user 0 com.sec.android.app.desktoplauncher
-adb shell pm disable-user --user 0 com.sec.android.app.dexonpc
-adb shell pm disable-user --user 0 com.sec.android.dexsystemui
-adb shell pm disable-user --user 0 com.sec.android.app.magnifier
-adb shell pm disable-user --user 0 com.sec.android.app.personalization
-adb shell pm disable-user --user 0 com.sec.app.RilErrorNotifier
-adb shell pm disable-user --user 0 com.sec.android.cover.ledcover
-adb shell pm disable-user --user 0 com.sec.android.desktopmode.uiservice
-adb shell pm disable-user --user 0 com.sec.android.emergencylauncher
-adb shell pm disable-user --user 0 com.sec.android.mimage.avatarstickers
-adb shell pm disable-user --user 0 com.sec.android.sdhms
-adb shell pm disable-user --user 0 com.sec.android.widgetapp.webmanual
-
-adb shell pm disable-user --user 0 com.sec.spp.push
-adb shell pm disable-user --user 0 com.swiftkey.swiftkeyconfigurator
-adb shell pm disable-user --user 0 com.touchtype.swiftkey
+for PACKAGE in "${PACKAGES_TO_DISABLE[@]}"; do
+    adb shell pm disable-user --user 0 "$PACKAGE"
+done
